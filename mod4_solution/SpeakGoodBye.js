f5c827edd62bf4760208a5e168b003f94b37bbf3
Ali Moonesian <33754968+Alimoonesian@users.noreply.github.com>
2017-11-27 03:05:57 +0330

    Update SpeakGoodBye.js

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -2,5 +2,6 @@ var byeSpeaker={};
 byeSpeaker.speakWord = "Good Bye";
 byeSpeaker.speak=function (name) {
   console.log(byeSpeaker.speakWord + " " + name);
+  var show= "<label style='color:red'>"+name+"</label>";
+  document.write("<br/>" + byeSpeaker.speakWord + " " +  show);
 }
-
