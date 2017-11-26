45d3bb4ab6e695ac41cf0dcb8c89529b5eb79ae2
Ali Moonesian <33754968+Alimoonesian@users.noreply.github.com>
2017-11-27 03:06:22 +0330

    Update SpeakHello.js

--- mod4_solution/SpeakHello.js
+++ mod4_solution/SpeakHello.js
@@ -2,4 +2,5 @@ var helloSpeaker={};
 helloSpeaker.speakWord = "Hello";
 helloSpeaker.speak=function speak(name) {
   console.log(helloSpeaker.speakWord + " " + name);
+  document.write("<br/>" + helloSpeaker.speakWord + " " + name);
 }
